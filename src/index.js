c6975ba8e823fa36fcb15565f168fd679914a35a
zhangyue <zygeilit@icloud.com>
2018-07-22 12:31:51 +0800

    new get pck info

--- src/index.js
+++ src/index.js
@@ -7,10 +7,19 @@ const fileNames = [ '.bscpmrc', '.bscpmrc.json', '.upm', '.upmrc' ]
 
 export const RCFileName = '.bscpmrc'
 
+// 获取package.json文件内容
+const getPackageInfo = () => {
+  const cpath = process.cwd()
+  let packInfo = {}
+  if (fs.existsSync(`${cpath}/package.json`)) {
+    packInfo = require(`${cpath}/package.json`)
+  }
+  return packInfo
+}
+
 export const extractRCFromPakcage = () => {
   const cpath = process.cwd()
-  const packInfo = require(`${cpath}/package.json`)
-  const { maintainers = [], name, description } = packInfo
+  const { maintainers = [], name, description } = getPackageInfo()
   // 使用 commonJs 规范提取组件维护者信息
   const developers = maintainers.map(developer => developer.name)
   // 从 package 中提取的配置信息
