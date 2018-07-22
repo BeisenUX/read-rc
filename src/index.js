c26ac212e4d1b2c7cd6c9d2b8947203221226fb0
zhangyue <zygeilit@icloud.com>
2018-07-22 16:03:28 +0800

    new

--- src/index.js
+++ src/index.js
@@ -8,20 +8,18 @@ const fileNames = [ '.bscpmrc', '.bscpmrc.json', '.upm', '.upmrc' ]
 export const RCFileName = '.bscpmrc'
 
 // 获取package.json文件内容
-const getPackageInfo = () => {
+export const getPackageInfo = () => {
   const cpath = process.cwd()
-  let packInfo = {}
-  if (fs.existsSync(`${cpath}/package.json`)) {
-    packInfo = require(`${cpath}/package.json`)
-  }
-  return packInfo
+  return fs.existsSync(`${cpath}/package.json`) && require(`${cpath}/package.json`) || {}
 }
 
 export const extractRCFromPakcage = () => {
   const cpath = process.cwd()
   const { maintainers = [], name, description } = getPackageInfo()
+
   // 使用 commonJs 规范提取组件维护者信息
   const developers = maintainers.map(developer => developer.name)
+
   // 从 package 中提取的配置信息
   return {
     'name': name,
---
+++ test/index.js
@@ -0,0 +1,29 @@
+
+const { spwanSync } = require('child_process')
+const assert = require('assert')
+const readRC = require('../lib').default
+const { extractRCFromPakcage, RCFileName, getPackageInfo } = require('../lib')
+
+describe('读取配置文件', () => {
+
+  process.chdir(`${__dirname}`)
+
+  it('获取package.json配置文件', () => {
+    let packinfo = getPackageInfo()
+    assert.equal(packinfo.main, 'lib/index.js')
+  })
+
+  it('输出正确的RC文件名', () => {
+    assert.equal(RCFileName, '.bscpmrc')
+  })
+
+  it('获取由Package.json文件生成的RC配置文件', () => {
+    let rc = extractRCFromPakcage()
+    assert.equal(rc.name, '@beisen/read-rc')
+  })
+
+  it('测试读取自定义RC配置文件', () => {
+    let rc = readRC()
+    assert.equal(rc.name, 'READRC')
+  })
+})
