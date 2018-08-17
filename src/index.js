96086d03fd5c22f3b2ed859a7e78db7ec0e72249
zhangyue <zygeilit@icloud.com>
2018-08-17 10:55:15 +0800

    new

--- src/index.js
+++ src/index.js
@@ -8,14 +8,14 @@ const fileNames = [ '.bscpmrc', '.bscpmrc.json' ]
 export const RCFileName = '.bscpmrc'
 
 // 获取package.json文件内容
-export const getPackageInfo = () => {
-  const cpath = process.cwd()
+export const getPackageInfo = (workspace) => {
+  const cpath = workspace || process.cwd()
   return fs.existsSync(`${cpath}/package.json`) && require(`${cpath}/package.json`) || {}
 }
 
-export const extractRCFromPakcage = () => {
-  const cpath = process.cwd()
-  const { maintainers = [], name, description } = getPackageInfo()
+export const extractRCFromPakcage = (workspace) => {
+  const cpath = workspace || process.cwd()
+  const { maintainers = [], name, description } = getPackageInfo(workspace)
 
   // 使用 commonJs 规范提取组件维护者信息
   const developers = maintainers.map(developer => developer.name)
@@ -34,13 +34,13 @@ export const extractRCFromPakcage = () => {
   }
 }
 
-export default () => {
-  const cpath = process.cwd()
+export default (workspace) => {
+  let cpath = workspace || process.cwd()
   let rc = {}
   for (let filename of fileNames) {
     if (fs.existsSync(`${cpath}/${filename}`)) {
       rc = Hjson.parse(fs.readFileSync(`${cpath}/${filename}`, 'utf-8'))
     }
   }
-  return Object.assign({}, extractRCFromPakcage(), rc)
+  return Object.assign({}, extractRCFromPakcage(workspace), rc)
 }
